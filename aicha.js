20c8ef9db3faa56ac1c3c1be13458db2824be690
Sebastian  Friedrich Nestler <151605820+Basty-devel@users.noreply.github.com>
2023-12-03 11:11:36 +0100

    Update aicha.js
    added fake email validation and email --> otp-server-->otp -->email from form input for 1st login

--- aicha.js
+++ aicha.js
@@ -7,18 +7,66 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   // Get the form data
-  const name = document.querySelector('#name').value;
-  const email = document.querySelector('#email').value;
-  const message = document.querySelector('#message').value;
+  const name = document.querySelector('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.querySelector('message').value;
 
   // Do something with the form data (e.g. send it to a server)
-  console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+  sendFormData(name, email, message);
 
   // Clear the form fields
-  document.querySelector('#name').value = '';
-  document.querySelector('#email').value = '';
-  document.querySelector('#message').value = '';
+  document.querySelector('name').value = '';
+  document.querySelector('email').value = '';
+  document.querySelector('message').value = '';
 });
+
+async function sendFormData(name, email, message) {
+  // Validate the email address
+  const url = 'https://emailvalidator3.p.rapidapi.com/validate';
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': process.env.API_KEY,
+      'X-RapidAPI-Host': 'emailvalidator3.p.rapidapi.com'
+    },
+    body: JSON.stringify({ email: email })
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    if (result.error) {
+      alert('Please don\'t use temp or fake emails!');
+    } else {
+      confirm('Thank you. The provided email does meet our requirements. You can go on with the registration process.');
+
+      // Send email to server and otp to email
+      const url = 'https://email-authentication-system.p.rapidapi.com/?recipient=' + encodeURIComponent(email) + '&app=Login%20System';
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': process.env.API_KEY,
+          'X-RapidAPI-Host': 'email-authentication-system.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        confirm(result);
+      } catch (error) {
+        alert(error);
+      }
+    }
+  } catch (error) {
+    alert(error);
+  }
+
+  console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+}
+
 // Generate a random alphanumeric string for the CAPTCHA
 function generateCaptcha() {
     const length = 5;
